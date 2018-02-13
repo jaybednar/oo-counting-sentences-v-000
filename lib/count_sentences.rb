f01c3c20630c56a992a0e29cf196ab6941240e25
Jay Bednar <ironviralmedia@gmail.com>
2018-02-13 22:56:02 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).count{|el| el != "" || el != " " || el != "!" || el != "." || el != "?"}
+    self.split(/[.!?]/).count{|el| el != ""}
   end
 end
